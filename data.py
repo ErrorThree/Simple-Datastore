d2a86af07bb1baa352853434260a9aba11d039b0
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-10-16 04:51:43 +0000

    Modified data.py

--- data.py
+++ data.py
@@ -5,46 +5,46 @@ import json
 def create(folder, jsonfile, template):
     jsonfile = str(jsonfile)
     template = dict(template)
-    if os.path.isfile(f"{folder}/{jsonfile}") or os.path.isfile(f"{jsonfile}"):
+
+    if os.path.isfile(f"{folder}/{jsonfile}") or os.path.isfile(jsonfile):
         print("File already exists.")
         return
-    elif folder == None:
-        f = open(f"{jsonfile}", "x")
+    elif folder is None:
+        f = open(jsonfile, "x")
         f.write(json.dumps(template))
         f.close()
         print("File created.")
         return
     elif os.path.isdir(folder):
-        f = open(f"{folder}/{jsonfile}", "x")
+        f = open(os.path.join(folder, jsonfile), "x")
         f.write(json.dumps(template))
         f.close()
-        print("File with in Folder created.")
+        print("File within folder created.")
         return
     else:
         os.makedirs(folder)
-        f = open(f"{folder}/{jsonfile}", "x")
+        f = open(os.path.join(folder, jsonfile), "x")
         f.write(json.dumps(template))
         f.close()
-        print("Folder and File created.")
+        print("Folder and file created.")
         return
 
 
 def read(folder, jsonfile):
     jsonfile = str(jsonfile)
-    f = open(f"{folder}/{jsonfile}", "r")
-    data = json.loads(f.read())
-    f.close()
+    with open(os.path.join(folder, jsonfile), "r") as f:
+        data = json.load(f)
     return data
 
 
 def edit(folder, jsonfile, update):
     update = dict(update)
-    f = open(f"{folder}/{jsonfile}", "rt").read()
-    unloadf = json.loads(f)
-    unloadf.update(update)
-    os.remove(f"{folder}/{jsonfile}")
-    f = open(f"{folder}/{jsonfile}", "x")
-    f.write(json.dumps(unloadf))
-    f.close()
+    file_path = os.path.join(folder, jsonfile)
+    with open(file_path, "rt") as f:
+        unloadf = json.load(f)
+        unloadf.update(update)
+    os.remove(file_path)
+    with open(file_path, "x") as f:
+        f.write(json.dumps(unloadf))
     print("File edited.")
     return
