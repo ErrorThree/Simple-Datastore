aaf09cf502070c60a713ea61a89aa6cb82caee99
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-10-16 05:13:15 +0000

    Added datastore_package/__init__.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datastore-package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package for creating, reading, and editing JSON files.",
+    long_description="A Python package for handling JSON data files.",
+    url="https://github.com/yourusername/datastore-package",
+)
